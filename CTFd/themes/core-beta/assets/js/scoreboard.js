f18c158098afbed10429b7a671afd5a480318687
lonnewolf120 <iftekharulislam360@gmail.com>
2024-09-07 15:33:56 +0600

    setup

--- CTFd/themes/core-beta/assets/js/scoreboard.js
+++ CTFd/themes/core-beta/assets/js/scoreboard.js
@@ -6,29 +6,16 @@ import { embed } from "./utils/graphs/echarts";
 window.Alpine = Alpine;
 window.CTFd = CTFd;
 
-// Default scoreboard polling interval to every 5 minutes
-const scoreboardUpdateInterval = window.scoreboardUpdateInterval || 300000;
-
 Alpine.data("ScoreboardDetail", () => ({
   data: {},
   show: true,
 
-  async update() {
+  async init() {
     this.data = await CTFd.pages.scoreboard.getScoreboardDetail(10);
 
-    let optionMerge = window.scoreboardChartOptions;
-    let option = getOption(CTFd.config.userMode, this.data, optionMerge);
-
+    let option = getOption(CTFd.config.userMode, this.data);
     embed(this.$refs.scoregraph, option);
     this.show = Object.keys(this.data).length > 0;
-  },
-
-  async init() {
-    this.update();
-
-    setInterval(() => {
-      this.update();
-    }, scoreboardUpdateInterval);
   },
 }));
 
@@ -37,21 +24,13 @@ Alpine.data("ScoreboardList", () => ({
   brackets: [],
   activeBracket: null,
 
-  async update() {
+  async init() {
     let response = await CTFd.fetch(`/api/v1/brackets?type=${CTFd.config.userMode}`, {
       method: "GET",
     });
     const body = await response.json();
     this.brackets = body["data"];
     this.standings = await CTFd.pages.scoreboard.getScoreboard();
-  },
-
-  async init() {
-    this.update();
-
-    setInterval(() => {
-      this.update();
-    }, scoreboardUpdateInterval);
   },
 }));
 
